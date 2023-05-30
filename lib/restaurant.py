bfe3b568b84d53bed709d9e164b11bf1a6af9a94
Erick <erickkirui653@gmail.com>
2023-05-30 12:05:50 -0400

    created review function

--- lib/restaurant.py
+++ lib/restaurant.py
@@ -4,3 +4,14 @@ class Restaurant:
      def __init__(self, name):
          self._name = name
          self.all_restaurants.append(self)
+    
+     def name(self):
+        return self._name
+     
+ 
+     def reviews(self):
+         restaurant_reviews = []
+         for review in Review.all():
+             if review.restaurant() == self:
+                 restaurant_reviews.append(review)
+         return [review.rating() for review in restaurant_reviews]
