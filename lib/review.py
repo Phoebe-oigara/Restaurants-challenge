ff411ca46b67623adc6f35a06777bea7508ce85c
Sang Kibet Elkana <elkanahkibet6@gmail.com>
2023-05-30 18:34:59 +0300

    Class method for that return all the reviews

--- lib/review.py
+++ lib/review.py
@@ -6,6 +6,12 @@ class Review:
         self._restaurant = restaurant
         self._rating = rating
         self.all_reviews.append(self)
+
     # rating method that return rating
     def rating(self):
         return self._rating
+    
+    #class method
+    @classmethod
+    def all(cls):
+        return cls.all_reviews
