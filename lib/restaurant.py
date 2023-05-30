824d636a7e8344ee141f8d5f0097860973b6212d
Erick <erickkirui653@gmail.com>
2023-05-30 12:08:56 -0400

    culculate average rating

--- lib/restaurant.py
+++ lib/restaurant.py
@@ -15,3 +15,19 @@ class Restaurant:
              if review.restaurant() == self:
                  restaurant_reviews.append(review)
          return [review.rating() for review in restaurant_reviews]
+
+     def average_star_rating(self):
+         total_ratings = 0
+         for review in self.reviews():
+             total_ratings += review.rating()
+         if len(self.reviews()) > 0:
+             return total_ratings / len(self.reviews())
+         else:
+             return 0.0
+         
+     
+    
+     
+
+     
+       
